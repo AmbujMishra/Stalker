6d85f74ef95ade78733ebd7f8cddcea6ec0117d1
Ambuj <hiambuj4u@gmail.com>
2015-01-28 00:15:38 +0530

    Update Stalker.java

--- Stalker.java
+++ Stalker.java
@@ -26,7 +26,7 @@ public class Stalker extends Actor {
 		setWidth(64);
 		setHeight(64);
 		//setPosition(Gdx.graphics.getWidth()/2- getWidth()/2,0 );		// Initial position of stalker, set accordingly
-		setPosition(sg.WIDTH/2-getWidth()/2, sg.HEIGHT/2-getHeight()/2);	//Independent of pixel
+		setPosition(sg.WIDTH/2- getWidth()/2,0 );	//Independent of pixel
 		setColor(Color.RED);	//Experiment: on this line
 		nY=getY()+sM;
 	}
